43e4986088fb0845a733a808dda977a2fc0f0336
tanveersaleem786 <tanveer.saleem786@gmail.com>
2019-12-20 13:31:15 -0500

    Stretch task done by Adding Quarter functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
 
   const handleHomeTouchDown = e => {
     setHomeScore(homeScore + 7);
@@ -22,6 +23,44 @@ function App() {
 
   const handleAwayFieldGoal = e => {
     setAwayScore(awayScore + 3);
+  };
+
+  const reset = e => {
+     setHomeScore(0);
+     setAwayScore(0); 
+     setQuarter(0);
+  };
+
+  const winner = homeScore > awayScore ? 'Home Team Wins' : 'Away Team Wins'
+
+  const handleNextQuarter = e => {
+
+    if(homeScore == awayScore && quarter == 4) {
+      
+       setQuarter("OT");
+
+    } else if(quarter === 4) {
+
+       alert(`Game is over and ${winner}`);
+       reset()
+
+    } else if(quarter === "OT") {
+
+       setQuarter(quarter + "2");
+
+    }  else if(quarter === "OT2") {
+
+          if(homeScore === awayScore) {
+            alert('Game is over in a Tie');
+            reset()
+          }
+          else {
+            alert(`The winner is ${winner}`);
+            reset()
+          }
+
+    } else setQuarter(quarter + 1);
+
   };
 
   return (
@@ -41,10 +80,10 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} />
       </section>
       <section className="buttons">
-        <div className="homeButtons">
+        <div className="homeButtons">          
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={handleHomeTouchDown}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={handleHomeFieldGoal}>Home Field Goal</button>
@@ -53,6 +92,7 @@ function App() {
           <button className="awayButtons__touchdown" onClick={handleAwayTouchDown}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={handleAwayFieldGoal}>Away Field Goal</button>
         </div>
+        <button onClick={handleNextQuarter}>Next Quarter</button>
       </section>
     </div>
   );
